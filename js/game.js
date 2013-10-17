f783704640675d028c76daf8841fe7b7f7df20d5
tkawabat <tosiki.kawabata@gmail.com>
2013-10-18 00:49:51 +0900

    修正

--- js/game.js
+++ js/game.js
@@ -2,10 +2,12 @@ var Higurashi = {};
 (function(NS) {
     NS.Const = {
         variables: ['person', 'weapon', 'place']
+        , person: ["カザック","ファスター","シセルド","ボビー","霧香"
+            ,"スーラ","三毛子","鮮血の鯨","ラセル","ツドイ"]
         , weapon: ["ナイフ","毒","銃","トンカチ","ガス","豆腐の角"
             ,"グレートソード","五寸釘","拳","アブナイ薬"]
-        , place: ["学校","病院","墓地","会社","戦場","船の上"
-            ,"自宅","旅館","天上界","長野"]
+        , place: ["学校","病院","墓地","協会","戦場","船の上"
+            ,"自宅","ディルス","天上界","長野"]
         , master: "ゲームマスター"
     };
     NS.Const.vnum = NS.Const.variables.length;
@@ -14,115 +16,137 @@ var Higurashi = {};
         function Game() {
             this.row_data;
             this.all_data;
-            this.your_data;
+            this.names;
         }
 
         Game.prototype.start = function(name, str, seed) {
-            var line = str.replace(new RegExp('　', 'g'), ' ').split('\n')
-            , mt = new jg.MT(seed)
-            , arr, arr2, i, n, key, num
+            var mt = new jg.MT(seed)
+            , bool, arr, arr2, i, n, key, num
             ;
 
             this.all_data = {};
             this.row_data = {};
+            this.names = [];
             for (i = 0; i < NS.Const.vnum; i++) {
                 this.all_data[NS.Const.variables[i]] = [];
                 this.row_data[NS.Const.variables[i]] = [];
             }
-            
-            // パース・シャッフル
-            for (i = 0; i < NS.Const.vnum; i++) {
-                key = NS.Const.variables[i];
 
-                if (i === 0) {
-                    if (typeof line[i] === 'undefined'
-                            || line[i].length === 0) {
-                        alert("ゲームデータを入れて下さい。やり直せ");
-                        return false;
-                    } else {
-                        arr = line[i].split(' ');
-                    }
-                    
-                    // ゲームマスター用の名前は使えない
-                    bool = arr.some(function(val) {
-                        return val === NS.Const.master
-                    });
-                    if (bool) {
-                        alert(NS.Const.master+"という名前は使えません。やり直せ。");
-                        return false;
-                    }
-
-                    // ゲームマスター追加
-                    arr.push(NS.Const.master);
-                    num = arr.length;
-                } else {
-                    if (typeof line[i] === 'undefined'
-                            || line[i].length === 0) {
-                        arr = [];
-                        arr2 = NS.Const[key].slice(0); //値移し
-                        while (arr.length < num) {
-                            NS.Util.rpick(arr2, arr, mt);
-                        }
-                    } else {
-                        arr = line[i].split(' ');
-                    }
-                    
-                    if (arr.length < num) {
-                        alert(key+'が'+arr.length+"個しかありません。\n"
-                            +num+"個必要です。やり直せ。");
-                        return false;
-                    }
-                }
-                
-                // 生データ
-                arr.forEach(function(val) {
-                    this.row_data[key].push(val);
-                }, this);
-                
-                // シャッフル
-                while (arr.length > 1) {
-                    NS.Util.rpick(arr, this.all_data[key], mt);
-                }
-                this.all_data[key].push(arr[0]);
+            // 空白処理
+            str = str.replace(new RegExp('　', 'g'), ' ')
+                    .replace(new RegExp(' +', 'g'), ' ')
+                    .trim()
+                    ;
+            if (str === '') {
+                alert("参加者を入れて下さい。やり直せ");
+                return false;
             }
             
-            // 自分のデータ作成
-            this.your_data = [];
-            n = this.all_data['person'].indexOf(name);
-
+            // パース
+            arr = str.split(' ');
+            
+            // ゲームマスター用の名前は使えない
+            bool = arr.some(function(val) {
+                return val === NS.Const.master
+            });
+            if (bool) {
+                alert(NS.Const.master+"という名前は使えません。やり直せ。");
+                return false;
+            }
+            
+            // 自分の名前があるかチェック
+            n = arr.indexOf(name);
             if (n === -1) {
                 alert("ゲーム内にあなたの名前がありません。やり直せ。");
                 return false;
             }
             
-            NS.Const.variables.forEach(function(key) {
-                this.your_data.push(this.all_data[key][n]);
-            }, this);
+            // ゲームマスター追加
+            arr.push(NS.Const.master);
+            num = arr.length;
+            
+            // シャッフル
+            for (i = 0; i < num; i++) {
+                NS.Util.rpick(arr, this.names, mt);
+            }
+            
+            // 各パラメタの値を持ってきてシャッフル
+            for (i = 0; i < NS.Const.vnum; i++) {
+                key = NS.Const.variables[i];
+                arr = [];
+                arr2 = NS.Const[key].slice(0); // 定数から値移し
+
+                // 個数チェック
+                if (arr2.length < num) {
+                    alert(key+'が'+arr2.length+"個しかありません。\n"
+                            +num+"個必要です。未対応です。");
+                    return false;
+                }
+
+                // 定数内からランダムピック
+                while (arr.length < num) {
+                    NS.Util.rpick(arr2, arr, mt);
+                }
+                
+                // シャッフル前のデータ作成
+                arr.forEach(function(val) {
+                    this.row_data[key].push(val);
+                }, this);
+                
+                // シャッフル
+                while (arr.length > 0) {
+                    NS.Util.rpick(arr, this.all_data[key], mt);
+                }
+            }
             
             return true;
         }
+        /* 名前を入れたらそのデータを配列で返す関数
+         * 
+         * @param {type} name
+         * @returns {Array} ret
+         */
+        Game.prototype.getMyData = function(name) {
+            ret = [];
+            n = this.names.indexOf(name);
+            if (n === -1) {
+                alert('システムエラーです。ごめんなさい\n');
+                ret = null;
+            } else {
+                NS.Const.variables.forEach(function(key) {
+                    ret.push(this.all_data[key][n]);
+                }, this);
+            }
+            return ret;
+        }
         Game.prototype.solve = function(str) {
-            var line = str.replace(new RegExp('　', 'g'), ' ').split(' ')
-            , variables = NS.Const.variables
-            , bool, i, n, key
+            var line
+            , arr, i
             ;
             
-            if (typeof this.all_data === 'undefined') {
-                alert('ゲームがスタートされていません。出直せ');
+            // 空白処理
+            str = str.replace(new RegExp('　', 'g'), ' ')
+                    .replace(new RegExp(' +', 'g'), ' ')
+                    .trim()
+                    ;
+            if (str === '') {
+                alert("入力がありません。やり直せ");
                 return false;
             }
+            
+            // パース
+            line = str.split(' ');
             
             // 個数チェック
-            if (line.length < NS.Const.vnum - 1) {
-                alert('なんか間違ってます。やり直せ');
+            if (line.length < NS.Const.vnum) {
+                alert(NS.Const.vnum+'個答えを入れて下さい。やり直せ');
                 return false;
             }
             
-            key = NS.Const.variables[0];
-            n = this.all_data[key].indexOf(NS.Const.master);
+            arr = this.getMyData(NS.Const.master);
             
-            for (i = 1; i < NS.Const.vnum; i++) {
-                if (line[i - 1] !== this.all_data[variables[i]][n]) {
+            for (i = 1; i < this.names.length; i++) {
+                if (line[i] !== arr[i]) {
                     alert('間違ってます');
                     return false;
                 }
@@ -182,11 +206,11 @@ var Higurashi = {};
     var game = new NS.Game()
     , util = NS.Util
     , d_game_init = $('#game_init')
-    , d_your_name  = d_game_init.find('#your_name')
-    , d_game_data  = d_game_init.find('#game_data')
-    , d_seed       = d_game_init.find('#random_seed')
-    , d_b_generate = d_game_init.find('#b_generate')
-    , d_b_start    = d_game_init.find('#b_start')
+    , d_your_name    = d_game_init.find('#your_name')
+    , d_player_names = d_game_init.find('#player_names')
+    , d_seed         = d_game_init.find('#random_seed')
+    , d_b_generate   = d_game_init.find('#b_generate')
+    , d_b_start      = d_game_init.find('#b_start')
 
     , d_game_on = $('#game_on')
     , d_all_data = d_game_on.find('#all_data')
@@ -203,33 +227,38 @@ var Higurashi = {};
     d_seed.val(Math.round(Math.random() * 1000000));
     
     // for debug
-    //d_game_data.val('aaa\nグレートソード　トンカチ\n長野　旅館');
+    //d_player_names.val('  　aaa 　bbb　 ccc  ');
     
     d_b_generate.click(function() {
         d_seed.val(Math.round(Math.random() * 1000000));
     });
     
     d_b_start.click(function() {
-        var arr;
+        var name = d_your_name.val()
+        , arr, n;
         
         // localstorageに保存
         util.set("your_name", d_your_name.val());
         
-        if (!game.start(d_your_name.val(), d_game_data.val(), +d_seed.val())) {
-            // エラー終了
-            return;
+        if (!game.start(name, d_player_names.val(), +d_seed.val())) {
+            return; // エラー終了
         }
         
         arr = [];
+        n = game.names.indexOf(name);
         NS.Const.variables.forEach(function(key, i) {
             arr[i] = util.addTag(key, {tag: 'th'})
                     + util.addTagMap(game.row_data[key], {tag: 'td'});
         });
         d_all_data.html(util.addTagMap(arr, {tag: 'tr'}));
+       
+        arr = game.getMyData(name);
+        
+        if (!arr) return;
         
         arr = [
             util.addTagMap(NS.Const.variables, {tag: 'th'})
-            , util.addTagMap(game.your_data, {tag: 'td'})
+            , util.addTagMap(arr, {tag: 'td'})
         ];
         
         d_your_data.html(util.addTagMap(arr, {tag: 'tr'}));
